b4ec92b7d2c2426dbb076a92750640c85c365d3f
alexwatts83 <alexjwatts83@gmail.com>
2020-04-10 00:48:42 +1000

    sidenav and toolbar wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fitness-tracker';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
 }
