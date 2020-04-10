625c760ae16d20b26f7ada47c02194340d90d993
alexwatts83 <alexjwatts83@gmail.com>
2020-04-10 17:45:37 +1000

    wip current training screen

--- src/app/materials-module.ts
+++ src/app/materials-module.ts
@@ -9,6 +9,11 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 const modules = [
   MatButtonModule,
@@ -20,7 +25,12 @@ const modules = [
   MatCheckboxModule,
   MatSidenavModule,
   MatToolbarModule,
-  MatListModule
+  MatListModule,
+  MatTabsModule,
+  MatCardModule,
+  MatSelectModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule
 ];
 const imports = [...modules];
 const exports = [...modules];
--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-current-training',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.scss']
 })
 export class CurrentTrainingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  progress: number;
+  timer: any;
+  message: string;
+  constructor() { 
+    this.progress = 0;
+    this.message = 'keep it 100';
   }
 
+  ngOnInit(): void {
+    this.timer = setInterval(()=>{
+      this.progress += 25;
+      if (this.progress >= 100) {
+        this.stopInterval('Exercised finished');
+      }
+    }, 1000);
+  }
+
+  onStopClicked() {
+    this.stopInterval('You stopped it');
+  }
+
+  stopInterval(msg: string) {
+    this.message = msg;
+    clearInterval(this.timer);
+  }
 }
